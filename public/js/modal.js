8b918edf81a84719ca586c950a9abd3df6874bae
Arkadeep Kashyap <kashyaparkadeep@gmail.com>
2021-03-26 10:15:26 +0530

    modal

--- public/js/modal.js
+++ public/js/modal.js
@@ -26,3 +26,38 @@ function openModal(){
 		}
 	} 
 }
+function myFunctionMobile() {
+	document.getElementById("myDropdownMobile").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+document.body.onclick = function (event) {
+	if (!event.target.matches('.dropbtn')) {
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+}
+function myFunction() {
+			document.getElementById("myDropdown").classList.toggle("show");
+		}
+
+		// Close the dropdown menu if the user clicks outside of it
+		document.body.onclick = function (event) {
+			if (!event.target.matches('.dropbtn')) {
+				var dropdowns = document.getElementsByClassName("dropdown-content");
+				var i;
+				for (i = 0; i < dropdowns.length; i++) {
+					var openDropdown = dropdowns[i];
+					if (openDropdown.classList.contains('show')) {
+						openDropdown.classList.remove('show');
+					}
+				}
+			}
+}
+
